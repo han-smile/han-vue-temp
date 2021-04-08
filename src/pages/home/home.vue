698c35f6322f971d021ea43031fa177ca4cd00bf
han-smile <macx@MacXdeMacBook.local>
2021-04-08 22:32:50 +0800

    init

---
+++ src/pages/home/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }} Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  mixins: [],
+  props: {
+    msg: {
+      type: String,
+      default: "Home",
+    },
+  },
+  data: function () {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.home {
+}
+</style>
